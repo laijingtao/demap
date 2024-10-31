a4bcba3957d7c1c907a35ededdd023b6ba2774d4
laijingtao <laijingtao@gmail.com>
2024-10-31 22:27:06 +0000

    project stream value to grid

--- demap/__init__.py
+++ demap/__init__.py
@@ -13,7 +13,8 @@ from .core import (process_dem,
                    extract_catchment_boundary)
 '''
 from .main import (process_dem, build_stream_network, extract_from_xy, split_stream_network,
-                   stream_coords_rowcol, stream_coords_xy, stream_coords_latlon)
+                   stream_coords_rowcol, stream_coords_xy, stream_coords_latlon,
+                   project_stream_to_grid)
 #from .chi import calculate_chi, calculate_chi_grid, calculate_ksn, calculate_channel_slope
 #from .valley import valley_xsec_at_xy, xsec_along_valley
 from .helpers import rowcol_to_xy, xy_to_rowcol, latlon_to_xy, xy_to_latlon
--- demap/main.py
+++ demap/main.py
@@ -1,5 +1,7 @@
+import numpy as np
 import xarray as xr
 
+from ._base import _speed_up
 from .xr_accessor import DemapDataarrayAccessor, DemapDatasetAccessor
 
 def process_dem(dem: xr.Dataset, **kwargs):
@@ -24,3 +26,103 @@ def stream_coords_xy(stream: xr.Dataset):
 
 def stream_coords_latlon(stream: xr.Dataset):
     return DemapDatasetAccessor(stream).stream_coords_latlon
+
+
+def project_stream_to_grid(dem_ds: xr.Dataset, stream_network_ds: xr.Dataset, var_name):
+
+    stream_coords_rows, stream_coords_cols = stream_coords_rowcol(stream_network_ds)
+
+    flow_dir_data = np.asarray(dem_ds['flow_dir'])
+    ordered_nodes_data = np.asarray(dem_ds['ordered_nodes'])
+
+    nearest_stream_node_row, nearest_stream_node_col = _assign_nearest_stream_node_impl(
+        flow_dir_data,
+        ordered_nodes_data,
+        np.asarray(stream_coords_rows),
+        np.asarray(stream_coords_cols)
+    )
+
+    gridded_value = _assign_stream_value_to_grid_impl(
+        nearest_stream_node_row,
+        nearest_stream_node_col,
+        stream_coords_rows,
+        stream_coords_cols,
+        np.asarray(stream_network_ds[var_name], dtype=np.float32), # assume value is float type
+        nodata=np.nan
+    )
+
+    return gridded_value
+
+
+@_speed_up
+def _assign_nearest_stream_node_impl(flow_dir: np.ndarray,
+                                     dem_ordered_nodes: np.ndarray,
+                                     stream_coords_rows: np.ndarray,
+                                     stream_coords_cols: np.ndarray,):
+    
+
+    nrow, ncol = np.max(dem_ordered_nodes[:, 0])+1, np.max(dem_ordered_nodes[:, 1])+1
+
+    nearest_stream_node_row = -np.ones((nrow, ncol), dtype=np.int32)
+    nearest_stream_node_col = -np.ones((nrow, ncol), dtype=np.int32)
+
+    #nearest_stream_node_row[stream_coords_rows, stream_coords_cols] = stream_coords_rows
+    #nearest_stream_node_col[stream_coords_rows, stream_coords_cols] = stream_coords_cols
+
+    for i, j in zip(stream_coords_rows, stream_coords_cols):
+        nearest_stream_node_row[i, j] = i
+        nearest_stream_node_col[i, j] = j
+
+    """
+    Demap's flow direction coding:
+    |4|3|2|
+    |5|0|1|
+    |6|7|8|
+    """
+    di = np.array([0, 0, -1, -1, -1, 0, 1, 1, 1])
+    dj = np.array([0, 1, 1, 0, -1, -1, -1, 0, 1])
+
+    for k in range(len(dem_ordered_nodes)-1, -1, -1):
+        i, j = dem_ordered_nodes[k, 0], dem_ordered_nodes[k, 1]
+
+        if nearest_stream_node_row[i, j] != -1:
+            # already assigned, meaning this node is a stream node
+            continue
+
+        if flow_dir[i, j] == 0:
+            # sink or outlet, nearest stream node is itself
+            nearest_stream_node_row[i, j] = i
+            nearest_stream_node_col[i, j] = j
+            continue
+        
+        r_i = i + di[flow_dir[i, j]]
+        r_j = j + dj[flow_dir[i, j]]
+
+        if r_i >= 0 and r_i < nrow and r_j >= 0 and r_j < ncol:
+            nearest_stream_node_row[i, j] = nearest_stream_node_row[r_i, r_j]
+            nearest_stream_node_col[i, j] = nearest_stream_node_col[r_i, r_j]
+        else:
+            # receiver is out the bound, skip
+            continue
+
+    return nearest_stream_node_row, nearest_stream_node_col
+
+
+def _assign_stream_value_to_grid_impl(nearest_stream_node_row: np.ndarray,
+                                      nearest_stream_node_col: np.ndarray,
+                                      stream_coords_rows: np.ndarray,
+                                      stream_coords_cols: np.ndarray,
+                                      value_along_stream: np.ndarray,
+                                      nodata):
+    # sink or outlet's nearest stream node is itself, so a nodata is needed for these nodes
+    # this means that these nodes are not included in the gridded value, may cause some problem in future
+
+    gridded_value = np.empty(nearest_stream_node_row.shape, dtype=value_along_stream.dtype)
+
+    gridded_value[:] = nodata
+
+    gridded_value[stream_coords_rows, stream_coords_cols] = value_along_stream
+
+    gridded_value = gridded_value[nearest_stream_node_row, nearest_stream_node_col]
+
+    return gridded_value
